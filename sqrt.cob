ba806f713f40cb5548c1a6435b0b82c3901d4ea1
Matthew Breckon <mbreckon@mail.uoguelph.ca>
2016-03-25 16:16:15 -0400

    Added comments. Fixed negative variable bug.

--- sqrt.cob
+++ sqrt.cob
@@ -1,80 +1,89 @@
 identification division.
-program-id. babylonian-sqrt.
+  program-id. babylonian-sqrt.
 
 environment division.
-input-output section.
-file-control.
-  select standard-output assign to display.
+  input-output section.
+    file-control.
+      select standard-output assign to display.
 
 data division.
-file section.
-fd standard-output.
-  01 out-line pic x(80).
-working-storage section.
-77 x    pic 9(11)v9(6).
-77 y    pic 9(11)v9(6).
-77 y2   pic 9(11)v9(6).
-77 n    pic 9(11)v9(6).
-01 in-card.
-  02 in-x     pic s9(11)v9(6).
-01 title-line.
-  02 filler pic x(9) value spaces.
-  02 filler pic x(26) value 'Square Root Approximations'.
-01 under-line.
-  02 filler pic x(45) value
-    '---------------------------------------------'.
-01 col-heads.
-  02 filler pic x(8) value spaces.
-  02 filler pic x(6) value 'Number'.
-  02 filler pic x(15) value spaces.
-  02 filler pic x(11) value 'Square Root'.
-01 under-line-2.
-  02 filler pic x(20) value '--------------------'.
-  02 filler pic x(5) value spaces.
-  02 filler pic x(20) value '--------------------'.
-01 print-line.
-  02 filler pic x value space.
-  02 out-x  pic z(11)9.9(6).
-  02 filler pic x(6) value spaces.
-  02 out-y  pic z(11)9.9(6).
-01 user-prompt.
-  02 filler pic x(31) value
-    'Input a value to be calculated:'.
-01 error-neg.
-  02 filler pic x(54) value
-    'Cannot calculate the square root of a negative number.'.
+  file section.
+    fd standard-output.
+      01 out-line pic x(80).
+  working-storage section.
+    *> Variables
+    77 in-x pic s9(11)v9(6).
+    77 x    pic 9(11)v9(6).
+    77 y    pic 9(11)v9(6).
+    77 y2   pic 9(11)v9(6).
+    77 n    pic 9(11)v9(6).
+    *> Message readouts
+    01 title-line.
+      02 filler pic x(9) value spaces.
+      02 filler pic x(26) value 'Square Root Approximations'.
+    01 under-line.
+      02 filler pic x(45) value
+        '---------------------------------------------'.
+    01 under-line-2.
+      02 filler pic x(20) value '--------------------'.
+      02 filler pic x(5) value spaces.
+      02 filler pic x(20) value '--------------------'.
+    01 col-heads.
+      02 filler pic x(8) value spaces.
+      02 filler pic x(6) value 'Number'.
+      02 filler pic x(15) value spaces.
+      02 filler pic x(11) value 'Square Root'.
+    01 print-line.
+      02 filler pic x value space.
+      02 out-x  pic z(11)9.9(6).
+      02 filler pic x(6) value spaces.
+      02 out-y  pic z(11)9.9(6).
+    01 user-prompt.
+      02 filler pic x(31) value
+        'Input a value to be calculated:'.
+    01 error-neg.
+      02 filler pic x(54) value
+        'Cannot calculate the square root of a negative number.'.
 
 procedure division.
-open output standard-output.
-user-input.
-  write out-line from under-line.
-  write out-line from title-line.
-  write out-line from under-line.
-  write out-line from user-prompt.
-  accept x.
-  if x < 0 then
-    write out-line from error-neg
-    stop run
-  end-if.
+  *> Open standard out
+  open output standard-output.
 
-sqrt.
-  compute y = x / 2.0.
-  compute y2 = y + 1.0.
-  perform calc until y = y2.
-  move x to out-x.
-  move y to out-y.
+  *> Prompt user for input
+  user-input.
+    write out-line from under-line.
+    write out-line from title-line.
+    write out-line from under-line.
+    write out-line from user-prompt.
+    accept in-x.
+    if in-x < 0 then
+      write out-line from error-neg
+      stop run
+    end-if.
+    move in-x to x.
 
-read-out.
-  write out-line from under-line.
-  write out-line from col-heads.
-  write out-line from under-line-2.
-  write out-line from print-line.
+  *> Perform babylonian square root
+  sqrt.
+    compute y = x / 2.0.
+    compute y2 = y + 1.0.
+    perform calc until y = y2.
+    move x to out-x.
+    move y to out-y.
 
-finish.
-  close standard-output.
-  stop run.
+  *> Output results
+  read-out.
+    write out-line from under-line.
+    write out-line from col-heads.
+    write out-line from under-line-2.
+    write out-line from print-line.
 
-calc.
-  compute n = x / y.
-  move y to y2.
-  compute y = (y + n) / 2.0.
+  *> Close standard out and end program
+  finish.
+    close standard-output.
+    stop run.
+
+  *> 'Function': Calculate recursive portion of babylonian square root
+  calc.
+    compute n = x / y.
+    move y to y2.
+    compute y = (y + n) / 2.0.
